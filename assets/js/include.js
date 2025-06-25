68a36540adc517de073e5b2b75501767a446c328
david <davidrdzb05@gmail.com>
2025-06-24 21:27:16 -0600

    añadir nav con js

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,3 +1,25 @@
+function includeNav() {
+  fetch("./assets/components/nav.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("nav").innerHTML = data;
+      const path = window.location.pathname.split("/").pop();
+      const navLinks = document.querySelectorAll("#nav a.nav-link");
+
+      navLinks.forEach(link => {
+        const href = link.getAttribute("href");
+        const trimmedhref = href.slice(2);
+
+        if (trimmedhref === path) {
+          link.classList.add("active-link");
+        }
+      });
+    })
+    .carch(error => {
+      console.error("Error cargando el nav:", error);
+    })
+}
+
 function includeFooter() {
   fetch("./assets/components/footer.html")
     .then(response => response.text())
@@ -5,8 +27,9 @@ function includeFooter() {
       document.getElementById("footer").innerHTML = data;
     })
     .catch(error => {
-      console.error('Error cargando el footer:', error);
+      console.error("Error cargando el footer:", error);
     });
 }
 
+document.addEventListener("DOMContentLoaded", includeNav)
 document.addEventListener("DOMContentLoaded", includeFooter)
