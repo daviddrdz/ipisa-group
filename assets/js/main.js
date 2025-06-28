d1c1a9188b6160329ebeda067445ba2fd84f7ab5
david <davidrdzb05@gmail.com>
2025-06-28 13:48:08 -0600

    agregar preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
 function botonContacto() {
     window.location.href = "./contacto.html";
 }
+
+window.addEventListener("load", () => {
+    const preloader = document.getElementById("preloader");
+    if (preloader) {
+        preloader.style.display = "none";
+    }
+});
