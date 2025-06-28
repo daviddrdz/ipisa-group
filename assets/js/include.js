9c94cd6ebeb564879845374cb3cdd8ca2199de6b
david <davidrdzb05@gmail.com>
2025-06-28 15:03:25 -0600

    fix navbar activelink

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,36 +1,39 @@
 function includeNav() {
-  fetch("./assets/components/nav.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("nav").innerHTML = data;
-      const path = window.location.pathname.split("/").pop().split(".")[0];
-      const navLinks = document.querySelectorAll("#nav a.nav-link");
+    fetch("./assets/components/nav.html")
+        .then((response) => response.text())
+        .then((data) => {
+            document.getElementById("nav").innerHTML = data;
+            const path = window.location.pathname.split("/").pop().split(".")[0];
+            const navLinks = document.querySelectorAll("#nav a.nav-link");
+            const index = document.getElementById("index");
 
-      navLinks.forEach(link => {
-        const href = link.getAttribute("href");
-        const trimmedhref = href.substring(href.lastIndexOf("/") + 1, href.lastIndexOf("."));
-        if (trimmedhref.includes(path)) {
-          link.classList.add("active-link");
-        }
-      });
-    })
-    .catch(error => {
-      console.error("Error cargando el nav:", error);
-    })
+            navLinks.forEach((link) => {
+                const href = link.getAttribute("href");
+                const trimmedhref = href.substring(href.lastIndexOf("/") + 1, href.lastIndexOf("."));
+                if (path == "") {
+                    index.classList.add("active-link");
+                } else if (trimmedhref.includes(path)) {
+                    link.classList.add("active-link");
+                }
+            });
+        })
+        .catch((error) => {
+            console.error("Error cargando el nav:", error);
+        });
 }
 
 function includeFooter() {
-  fetch("./assets/components/footer.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("footer").innerHTML = data;
-    })
-    .catch(error => {
-      console.error("Error cargando el footer:", error);
-    });
+    fetch("./assets/components/footer.html")
+        .then((response) => response.text())
+        .then((data) => {
+            document.getElementById("footer").innerHTML = data;
+        })
+        .catch((error) => {
+            console.error("Error cargando el footer:", error);
+        });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  includeNav();
-  includeFooter();
-})
+    includeNav();
+    includeFooter();
+});
