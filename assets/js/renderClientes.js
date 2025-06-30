c1973f9c2d0fe43a90a5fcdc65c1f698b4351814
david <davidrdzb05@gmail.com>
2025-06-29 23:41:42 -0600

    hacer responsive

--- assets/js/renderClientes.js
+++ assets/js/renderClientes.js
@@ -7,7 +7,7 @@ function renderClientes() {
         row.className = "row d-flex align-items-center";
         for (let j = 0; j < 4 && i < clientes.length; i++, j++) {
             let cliente = `
-                <div class="col d-flex justify-content-center m-2">
+                <div class="col d-flex justify-content-center m-2 clientes">
                     <img src="${clientes[i].img}">
                 </div>
                 `;
