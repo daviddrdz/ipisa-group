dcd07968ae6c0f18902ad6277c2eaf14c12452d8
david <davidrdzb05@gmail.com>
2025-11-07 16:24:37 -0600

    añadir informacion categorias

---
+++ assets/js/data/categories.js
@@ -0,0 +1,37 @@
+export const categories = [
+    {
+        category: "Básculas de cinta transportadora",
+        img: "./assets/img/products/cintas_transportadoras/N-64-1.jpg",
+        url: "./cintas_transportadoras.html"
+    },
+    {
+        category: "Interruptores para cintas transportadoras",
+        img: "./assets/img/products/interruptores_cintas_transportadoras/BD-3_1.jpg",
+        url: "./interruptores_cintas.html"
+    },
+    {
+        category: "Detección de nivel de punto",
+        img: "./assets/img/products/deteccion_nivel_punto/Rotary-Level-Switches_1.jpg",
+        url: "./deteccion_nivel.html"
+    },
+    {
+        category: "Interruptores de velocidad",
+        img: "./assets/img/products/interruptores_velocidad/PL72-115_1.jpg",
+        url: "./interruptores_velocidad.html"
+    },
+    {
+        category: "Detectores de metales",
+        img: "./assets/img/products/detectores_metales/Model-50_2.jpg",
+        url: "./detectores_metales.html"
+    },
+    {
+        category: "Muestradores",
+        img: "./assets/img/products/muestradores/1.png",
+        url: "./muestradores.html"
+    },
+    {
+        category: "Dosificadores",
+        img: "./assets/img/products/dosificadores/1.png",
+        url: "./dosificadores.html"
+    }
+]
