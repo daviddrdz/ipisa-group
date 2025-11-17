cbb5c55ab409ecc82ed8ced9b972b880f6df3754
david <davidrdzb05@gmail.com>
2025-11-17 15:07:04 -0600

    modificar renderizado de categorias

---
+++ assets/js/data/categorias.js
@@ -0,0 +1,37 @@
+export const categorias = [
+    {
+        nombre: "Básculas de cinta transportadora",
+        img: "/assets/img/products/cintas_transportadoras/N-64-1.jpg",
+        url: "/productos/basculas-cintas-transportadoras.html"
+    },
+    {
+        nombre: "Interruptores para cintas transportadoras",
+        img: "/assets/img/products/interruptores_cintas_transportadoras/BD-3_1.jpg",
+        url: "/productos/interruptores-cintas-transportadoras.html"
+    },
+    {
+        nombre: "Detección de nivel de punto",
+        img: "/assets/img/products/deteccion_nivel_punto/Rotary-Level-Switches_1.jpg",
+        url: "/productos/deteccion-nivel-punto.html"
+    },
+    {
+        nombre: "Interruptores de velocidad",
+        img: "/assets/img/products/interruptores_velocidad/PL72-115_1.jpg",
+        url: "/productos/interruptores-velocidad.html"
+    },
+    {
+        nombre: "Detectores de metales",
+        img: "/assets/img/products/detectores_metales/Model-50_2.jpg",
+        url: "/productos/detectores-metales.html"
+    },
+    {
+        nombre: "Muestreadores",
+        img: "/assets/img/products/muestreadores/1.png",
+        url: "/productos/muestreadores.html"
+    },
+    {
+        nombre: "Dosificadores",
+        img: "/assets/img/products/dosificadores/F52.png",
+        url: "/productos/dosificadores.html"
+    }
+]
---
+++ assets/js/features/renderCategorias.js
@@ -0,0 +1,21 @@
+import { categorias } from "/assets/js/data/categorias.js";
+
+function redirectCategorias(url) {
+    window.location.href = url;
+}
+
+function renderCategorias() {
+    const container = document.getElementById("categorias");
+    for (let i = 0; i < categorias.length;i++) {
+        let categoria = document.createElement("div");
+        categoria.className = "categoria";
+        categoria.onclick = () => redirectCategorias(categorias[i].url);
+        categoria.innerHTML = `
+            <img src="${categorias[i].img}" class="categoria__img" alt=${categorias[i].nombre}">
+            <p class="categoria__nombre">${categorias[i].nombre}</p>
+        `;
+        container.appendChild(categoria);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", renderCategorias);
